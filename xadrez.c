c48eeb155a89445cee53f48f7091b15d9fbf8b0b
biabuck <beatrizbuck300@gmail.com>
2025-03-20 18:07:44 +0000

    Finalização Xadrez (Recursividade)

--- xadrez.c
+++ xadrez.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
+void torre(int casas){
+
+    if (casas > 0)
+    {
+        printf("Direita \n");
+        torre(casas - 1);
+    }
+}
+
+void bispo(int casas){
+
+    if (casas > 0)
+    {
+        printf("Direita, Cima \n");
+        bispo(casas - 1);
+    }
+}
+
+void rainha(int casas){
+
+    if (casas > 0)
+    {
+        printf("Esquerda \n");
+        rainha(casas - 1);
+    }
+}
+
 int main(){
 
     int pecas;
-    int i = 1;
-    int direcaoB; 
-    int j = 1;
-
+    
     printf("Bem-Vindo ao jogo de Xadrez!!! \n");
     printf("Escolha uma peça: \n");
     printf("1. Torre \n");
@@ -15,95 +39,38 @@ int main(){
     printf("4. Cavalo \n");
     printf("Digite sua escolha: ");
     scanf("%d", &pecas);
+    printf("\n");
 
     switch (pecas)
     {
-        //Código da Torre
-        case 1: 
-            for (int i = 0; i < 5; i++)
+    case 1:
+        torre(5);
+    break;
+
+    case 2:
+        bispo(5);
+    break;
+
+    case 3:
+        rainha(8);
+    break;
+
+    case 4:
+        for (int i = 1, j = 1; i <= 1; i++)
+        {
+            while (j <= 2)
             {
-                printf("Direita \n");
+                printf("Cima \n");
+                j++;
             }
-        break;
+            
+            printf("Direita \n");
+        }
+    break;
 
-        //Código do Bispo
-        case 2:
-        printf("Escolha uma direção: \n");
-        printf("1. Cima, Direita \n");
-        printf("2. Cima, Esquerda \n");
-        printf("3. Baixo, Direita \n");
-        printf("4. Baixo, Esquerda\n");
-        printf("Digite sua escolha: ");
-        scanf("%d", &direcaoB);
-
-            //Escolha de direção do Bispo
-            switch (direcaoB)
-            {
-                case 1:
-                    while (i <= 5)
-                    {
-                        printf("Cima, Direita \n");
-                        i++;
-                    }
-                    break;
-
-                case 2:
-                    while (i <= 5)
-                    {
-                        printf("Cima, Esquerda \n");
-                        i++;
-                    }
-                    break;
-
-                case 3:
-                    while (i <= 5)
-                    {
-                        printf("Baixo, Direita \n");
-                        i++;
-                    }
-                    break;
-
-                case 4:
-                    while (i <= 5)
-                    {
-                        printf("Baixo, Esquerda \n");
-                        i++;
-                    }
-                    break;
-                        
-                default:
-                    printf("Opção inválida");
-                break;
-            }
-        break;
-
-
-        //Código da Rainha
-        case 3: 
-            do
-            {
-                printf("Esquerda \n", i);
-                i++;
-            } while (i <= 8);
-        break;
-
-        // Código do cavalo (estrutura aninhada)
-        case 4: 
-            for (int i = 1; i <= 1; i++)
-            {
-                while (j <= 2)
-                {
-                    printf("Cima \n");
-                    j++;
-                }
-    
-                printf("Direita \n");
-            }
-        break;
-    
-        default:
-        printf("Opção inválida");
-            break; 
+    default:
+        printf("Opção inválida \n");
+    break;
     }
 
 return 0;
